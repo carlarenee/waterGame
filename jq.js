2af824311600a919b6a438e322b5935365d8dd82
carla stockalper <cstockalper@gmail.com>
2016-10-05 13:07:25 -0400

    add page 1 with input form

--- jq.js
+++ jq.js
@@ -1,20 +1,38 @@
 $(document).ready(function() {
 
+var start = $('#start');
 // Turn cursor into weapon when hovering over a div
 var wholePage = $('body');
-// var circle = $('#target');
-// var crossHair = function () {
-//     circle.css( 'cursor' , 'url(assets/pow-01.png), crossHair');
-//   };
 var crossHair = function () {
-    wholePage.css( 'cursor' , 'url(assets/pow-01.png), crossHair');
+    wholePage.css( 'cursor' , 'url(assets/pow.png), crossHair');
   };
+// variables
+var mon1 = $('#monster1');
+var badGuy = $('.badGuys')
+//functions
+var swimMonsterLeft = function() {
+  var distance = 1000;
+  setInterval(function() {
+    mon1.css('left', distance + 'px');
+    if (distance < 0) {
+      distance = 920;
+    } else {
+      distance -= 5;
+    }
+  }, 80);
+};
+
+var blowup = function() {
+  $(this).css('background-color', 'white');
+}
 
 
 
-// Call functions here
-//circle.hover(crossHair);
+
 wholePage.hover(crossHair);
+start.click(swimMonsterLeft);
+mon1.click(blowup);
+$('#monster2')click(blowup);
 
 });
 
