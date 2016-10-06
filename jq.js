357462d2488c1bfeb0be4c4709b9e9554b9c57ee
carla stockalper <cstockalper@gmail.com>
2016-10-06 16:27:54 -0400

    work on displaying 30 second countdown timer

--- jq.js
+++ jq.js
@@ -9,19 +9,24 @@ $('#playerName').append(name[1]);
 console.log(name);
 
 var start = $('#start');
+
 // Turn cursor into weapon when hovering over a div
 var wholePage = $('body');
 var crossHair = function () {
     wholePage.css( 'cursor' , 'url(assets/pow.png), crossHair');
   };
+
 // variables
 var mon1 = $('#monster1');
-var badGuy = $('.badGuys')
-//functions
+var mon2 = $('#monster2');
+var mon3 = $('#monster3');
+var mon4 = $('#monster4');
+var badGuy = $('.badGuys');
 
-
-var swimMonsterLeft = function() {
-  var distance = 1000;
+// movement functions
+// monster to swim from right to left
+var swim1 = function() {
+  var distance = 920;
   setInterval(function() {
     mon1.css('left', distance + 'px');
     if (distance < 0) {
@@ -29,20 +34,79 @@ var swimMonsterLeft = function() {
     } else {
       distance -= 5;
     }
-  }, 80);
+  }, 40);
 };
 
+// monster to swim left to right
+var swim2 = function() {
+  var distance = 0;
+  setInterval(function() {
+    mon2.css('left', distance + 'px');
+    if (distance > 920) {
+      distance = 0;
+    } else {
+      distance += 5;
+    }
+  }, 20);
+};
+
+var swim3 = function() {
+  var distance = 920;
+  setInterval(function() {
+    mon3.css('left', distance + 'px');
+    if (distance < 0) {
+      distance = 920;
+    } else {
+      distance -= 5;
+    }
+  }, 10);
+};
+
+var swim4 = function() {
+  var distance = 0;
+  setInterval(function() {
+    mon4.css('left', distance + 'px');
+    if (distance > 920) {
+      distance = 0;
+    } else {
+      distance += 10;
+    }
+  }, 10);
+};
+
+
+// shoot functions
 var blowup = function() {
   $(this).css('background-color', 'white');
+}
+
+// Timer
+var $seconds = 30;
+var $display = $('#playerTime')
+setInterval(function(){ countDown }, 1000);
+
+var countDown = function() {
+  $seconds--;
+  if ($seconds === 0) {
+    clearInterval(countDown);
+  }
+  $display.text($seconds + " seconds");
 }
 
 
 
 
-wholePage.hover(crossHair);
-start.click(swimMonsterLeft);
-mon1.click(blowup);
 
+
+// event listeners
+//wholePage.hover(crossHair);
+start.click(swim1);
+start.click(swim2);
+start.click(swim3);
+start.click(swim4);
+mon1.click(blowup);
+mon2.click(blowup);
+start.click(countDown);
 
 });
 
