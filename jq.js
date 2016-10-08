4054a941d9f7b8f137db0fbd198ce928a6f80347
carla stockalper <cstockalper@gmail.com>
2016-10-08 17:05:59 -0400

    broke everything, revert to previous code, add reset to exploded enemies

--- jq.js
+++ jq.js
@@ -3,20 +3,19 @@ $(document).ready(function() {
 //getting input from page 1 into page 2
 var url = window.location.search.substring(1);
 var retrieve = url.split('&');
-console.log(retrieve);
 var name = retrieve[0].split('=');
 $('#playerName').append(name[1]);
-console.log(name);
 
 var start = $('#start');
 
 // Turn cursor into weapon when hovering over a div
-var wholePage = $('body');
-var crossHair = function () {
-    wholePage.css( 'cursor' , 'url(assets/pow.png), crossHair');
-  };
+// var wholePage = $('body');
+// var crossHair = function () {
+//     wholePage.css( 'cursor' , 'url(assets/pow.png), crossHair');
+//   };
 
 // variables
+
 var score = 0;
 var mon1 = $('#monster1');
 var mon2 = $('#monster2');
@@ -25,8 +24,7 @@ var mon4 = $('#monster4');
 var friend = $('#friend');
 var badGuy = $('.badGuys');
 
-// movement functions
-// monster to swim from right to left
+// movement functions, set fish swimming from side to side at varying speeds.
 var swim1 = function() {
   var distance = 920;
   setInterval(function() {
@@ -38,8 +36,6 @@ var swim1 = function() {
     }
   }, 40);
 };
-
-// monster to swim left to right
 var swim2 = function() {
   var distance = 0;
   setInterval(function() {
@@ -86,15 +82,28 @@ var swimfriend = function() {
 };
 
 // reset function to turn bad fish back to fish after they explode
-var reset = function() {
+var reset1 = function() {
   mon1.css('background-image', 'url(assets/badfish.gif');
 }
-// explosion animation when bad fish are hit, then reset
-var blowup = function() {
-  $(this).css('background-image', 'url(assets/explode.gif');
-  setTimeout(reset, 1000);
+var reset2 = function() {
+  mon2.css('background-image', 'url(assets/squid.gif');
 }
-
+var reset3 = function() {
+  mon3.css('background-image', 'url(assets/squid.gif');
+}
+// explosion animation when bad fish are hit, then reset
+var blowup1 = function() {
+  $(this).css('background-image', 'url(assets/explode.gif');
+  setTimeout(reset1, 1000);
+}
+var blowup2 = function() {
+  $(this).css('background-image', 'url(assets/explode.gif');
+  setTimeout(reset2, 1000);
+}
+var blowup3 = function() {
+  $(this).css('background-image', 'url(assets/explode.gif');
+  setTimeout(reset3, 1000);
+}
 
 
 // adding and subtracting points
@@ -127,14 +136,14 @@ var hitfriend = function() {
 }
 
 // Timer
-var $seconds = 5;
-var $display = $('#playerTime')
+var $seconds = 20;
+var $display = $('#playerTime');
 var countDown = function() {
   setInterval(function(){
   $seconds--;
   if ($seconds < 0) {
     clearInterval();
-    if ($score > 5) {
+    if ($score >= 30) {
       console.log('winner');
       $('#winner').animate({
         height: '500px',
@@ -148,37 +157,28 @@ var countDown = function() {
       }
     return;
     }
-
   $display.text($seconds + " seconds");
   }, 1000);
 }
 
-// win or lose
-// if ($score > 5) {
-//       console.log('winner');
-//       $('#winner').animate({
-//         height: '500px',
-//         width : '500px'
-//       }, 3000);}
-
 // event listeners
 //wholePage.hover(crossHair);
+start.click(countDown);
 start.click(swim1);
 start.click(swim2);
 start.click(swim3);
 start.click(swim4);
 start.click(swimfriend);
-mon1.click(blowup);
+mon1.click(blowup1);
 mon1.click(hitMon1);
-mon2.click(blowup);
+mon2.click(blowup2);
 mon2.click(hitMon2);
-mon3.click(blowup);
+mon3.click(blowup3);
 mon3.click(hitMon3);
 mon4.click(blowup);
 mon4.click(hitMon4);
 friend.click(blowup);
 friend.click(hitfriend);
-start.click(countDown);
 });
 
 
